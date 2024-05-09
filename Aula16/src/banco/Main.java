140fba7266cf2c3ad3158debdaa12c600224fd36
vitorvhsilva <dndragonbr@gmail.com>
2024-05-09 20:40:44 -0300

    Exercício da Aula16

--- Aula16/src/banco/Main.java
+++ Aula16/src/banco/Main.java
@@ -7,19 +7,26 @@ import banco.exceptions.SaqueInvalidoException;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		ContaCorrente contaDoTonyStark = new ContaCorrente(10);
+
+		// RuntimeException deve ser usada quando a exceção pode ser prevenida,
+		// Ja para erros que devem ser prevenidos usa-se a Exception
+
+		// RuntimeException eh unchecked
+		// Ja Exception eh checked, obrigando quem chama esse método a dar try-catch ou throws
+
+		// Classes que usam Excecoes que herdam do Exception obrigatoriamente precisam de throws apos o argumento
+		// Ja das que herdam RuntimeException, nao e necesarrio
+
+		ContaCorrente contaDoTonyStark = new ContaCorrente(100000);
 		try {
-			contaDoTonyStark.sacar(5);
-		}
-		catch (SaqueInvalidoException e) {
+			contaDoTonyStark.sacar(10);
+		} catch (SaqueInvalidoException e) {
 			System.out.println(e.getMessage());
 		}
 
 		try {
-			contaDoTonyStark.depositar(-20);
-		}
-		catch (DepositoInvalidoException e) {
+			contaDoTonyStark.depositar(-1);
+		} catch (DepositoInvalidoException e) {
 			System.out.println(e.getMessage());
 		}
 
