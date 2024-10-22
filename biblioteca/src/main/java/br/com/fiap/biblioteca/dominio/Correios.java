c94b7c1c49bb8b55ecb90c821d78bc617b2c17b4
vitorvhsilva <88109337+vitorvhsilva@users.noreply.github.com>
2024-10-21 21:23:36 -0300

    Delete biblioteca directory

--- biblioteca/src/main/java/br/com/fiap/biblioteca/dominio/Correios.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap.biblioteca.dominio;
-
-public interface Correios {
-    public Endereco buscarEnderecoPor(String cep);
-}
--- biblioteca/src/main/resources/cria_tabela_alunos.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE alunos (
-	id LONG PRIMARY KEY AUTO_INCREMENT, 
-	nome VARCHAR(100), 
-	chamada VARCHAR(6),
-	turma VARCHAR(6));
